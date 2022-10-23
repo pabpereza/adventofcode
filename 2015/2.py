17be8e5900838675b9b1f173eb4256c2d5f6f57d
pabpereza <pabloperezaradros@gmail.com>
2022-10-23 20:19:34 +0200

    Solved challenges 1 and 2 of 2015 year

---
+++ 2015/2.py
@@ -0,0 +1,35 @@
+"""
+--- Day 2: I Was Told There Would Be No Math ---
+The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
+
+Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
+
+For example:
+
+A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
+"""
+
+data = ""
+total = 0
+
+with  open ( "2_input" ,  "r" )  as  file:
+    data = file.read()
+
+def count_feets(l,w,h):
+    area = []
+    area.append(2*l*w)
+    area.append(2*w*h)
+    area.append(2*h*l)
+    area.sort()
+    
+    paper = area[0]/2 + area[0] + area[1] + area[2]
+    return paper
+
+for line in data.split("\n"):
+    if line != "":
+        l,w,h = line.split("x")
+        total += count_feets(int(l),int(w),int(h))
+
+print(total)
